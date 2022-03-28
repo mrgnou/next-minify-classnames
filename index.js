c4507734331678015bb805544b75ae232a8f3ad1
Leo Cheron <leo@cheron.works>
2022-03-28 18:43:46 +0200

    fix: add test if use is not iterable

--- index.js
+++ index.js
@@ -3,48 +3,46 @@ const getMinifiedIdent = require('./get-minified-ident')
 const LETTER = /[A-Za-z]/
 
 module.exports = (config, {
-  dictionary = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ0123456789',
-  enable = process.env.NODE_ENV === 'production',
-  prefix = '',
-  suffix = ''
+dictionary = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ0123456789',
+enable = process.env.NODE_ENV === 'production',
+prefix = '',
+suffix = ''
 }) => {
-  if (!enable) {
-    return config
-  } else if (!LETTER.test(dictionary)) {
-    throw new Error('"dictionary" option must have at least one letter')
-  }
+if (!enable) {
+	return config
+} else if (!LETTER.test(dictionary)) {
+	throw new Error('"dictionary" option must have at least one letter')
+}
 
-  return {
-    ...config,
+return {
+	...config,
 
-    webpack: (webpackConfig, ...rest) => {
-      for (const { oneOf } of webpackConfig.module.rules) {
-        if (!oneOf?.length) {
-          continue
-        }
+	webpack: (webpackConfig, ...rest) => {
+	for (const { oneOf } of webpackConfig.module.rules) {
+		if (!oneOf?.length) continue
 
-        for (const { use } of oneOf) {
-          if (!use) {
-            continue
-          }
+		for (const { use } of oneOf) {
+			if (!use || !(Symbol.iterator in Object(use))) {
+				continue
+			}
 
-          for (const { loader, options } of use) {
-            if (!loader?.includes('/css-loader/') || !options?.modules) {
-              continue
-            }
+			for (const { loader, options } of use) {
+				if (!loader?.includes('/css-loader/') || !options?.modules) {
+				continue
+				}
 
-            options.modules.getLocalIdent = (context, _, localName) => {
-              const identParts = { path: context.resourcePath, localName }
-              const minifiedIdent = getMinifiedIdent(identParts, dictionary)
-              return `${prefix}${minifiedIdent}${suffix}`
-            }
-          }
-        }
-      }
+				options.modules.getLocalIdent = (context, _, localName) => {
+				const identParts = { path: context.resourcePath, localName }
+				const minifiedIdent = getMinifiedIdent(identParts, dictionary)
+				return `${prefix}${minifiedIdent}${suffix}`
+				}
+			}
+		}
+	}
 
-      return typeof config.webpack === 'function'
-        ? config.webpack(webpackConfig, ...rest)
-        : webpackConfig
-    }
-  }
+	return typeof config.webpack === 'function'
+		? config.webpack(webpackConfig, ...rest)
+		: webpackConfig
+	}
+}
 }
